6d5183261f52c5ae2ab9b74ea3015d5fa2fd4067
Theogama <sandilegama.sg@gmail.com>
2024-07-07 20:55:06 +0200

    second content update

--- components/ui/GridGlobe.tsx
+++ components/ui/GridGlobe.tsx
@@ -401,7 +401,7 @@ const GridGlobe = () => {
       {/* remove h-full md:h-[40rem] */}
       <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-96 px-4">
         {/* remove these text divs */}
-         <motion.div
+         {/* <motion.div
           initial={{
             opacity: 0,
             y: 20,
@@ -422,7 +422,7 @@ const GridGlobe = () => {
             This globe is interactive and customizable. Have fun with it, and
             don&apos;t forget to share it.
           </p>
-        </motion.div>
+        </motion.div> */}
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
         {/* remove -bottom-20 */}
         <div className="absolute w-full h-72 md:h-full z-10">
